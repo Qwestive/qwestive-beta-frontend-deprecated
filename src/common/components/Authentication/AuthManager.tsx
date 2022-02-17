2eb004af575be11cc67e8a7893b44b4fa19ee1c2
JINDBS <jean.duquenne.ch@gmail.com>
2022-02-17 04:01:46 +0100

    removed comment

--- src/common/components/Authentication/AuthManager.tsx
+++ src/common/components/Authentication/AuthManager.tsx
@@ -33,9 +33,7 @@ export default function AuthManager({
 }: {
   children: ReactNode;
 }): JSX.Element {
-  // const { disconnect, connected } = useWallet();
   const { publicKey, signMessage, disconnect, connected } = useWallet();
-
   const [userPublicKey, setUserPublickey] = useRecoilState(userPublicKeyAtom);
   const [, setUserName] = useRecoilState(userNameAtom);
   const [, setDisplayName] = useRecoilState(userDisplayNameAtom);
