fe1fea16660a864be12121109cab05e2be8e9443
metadiego <diegoolalde@gmail.com>
2022-03-14 16:31:47 -0400

    Cleanup of send tip logic

--- src/pages/Community/PostDetail/PostDetailPage.tsx
+++ src/pages/Community/PostDetail/PostDetailPage.tsx
@@ -5,8 +5,6 @@ import CommentSection from './components/CommentSection';
 import { IpostArticle } from '../../../common/types';
 import PostActionsSection from './components/PostActionsSection';
 import { getPostInfo } from '../../../common/services/Firebase/GetData/PostUtils';
-import TipModal from './components/TipModal';
-import { SendTipButton } from './components/SendTipButton';
 import TipModalContainer from './components/TipModalContainer';
 
 /// Component which shows all of the information inside a post.
