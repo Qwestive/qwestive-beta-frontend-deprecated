a7e574d0b5cd548e30517a67b293e8a66c56748b
metadiego <diegoolalde@gmail.com>
2022-03-15 13:15:40 -0400

    Config for setting up a vote

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const appConfig = {
   SOLANA_NETWORK_COMMITMENT: 'confirmed',
   LANDING_PAGE_SIGN_IN_ENABLED: true,
   LANDING_PAGE_SHOW_COMMUNITIES_SECTION: false,
+  VOTING_CONTRACT_PUBLIC_KEY: 'C7WtvPweJYUgy72cFzbmmgs6nZYNX1SWov6Dniqi7kXK',
 };
 
 export default appConfig;
